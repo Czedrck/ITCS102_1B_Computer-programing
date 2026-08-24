2b43089e73fd93acd1e48871cbc446988549bde0
Czedrck <cedricklipata1@gmail.com>
2026-08-24 10:11:57 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,27 @@
+#using variables
+
+#int
+a1 = 18
+a2 = 28
+a3 = 38	
+a4 = 48
+
+#float
+temp1 = 35.5
+temp2 = 35.6	
+temp3 = 35.7
+temp4 = 35.8
+
+#string
+name1 = "jhon"
+name2 = "cedrick"
+name3 = "frias"
+name4 = "lipata"
+
+#boolean
+isStudent = True
+
+print("My age is", a1, a2, a3, a4)
+print("The room temperature is", temp1, temp2, temp3, temp4)
+print("My complete name is", name1, name2, name3, name4)
+
