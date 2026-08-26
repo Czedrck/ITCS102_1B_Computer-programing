3da784a7cb6df571c2c368a969a20309c7663578
Czedrck <cedricklipata1@gmail.com>
2026-08-26 11:28:57 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,10 @@
+#escape sequence
+# \n = new line
+# \t = tab space
+# \r = carraige return\deprecated
+# \\ = insert slash
+# \" = insert double slash
+
+phrase1 = "\b You can be happy in love with me this instant, it is ok to let go,to be happy"
+
+print(phrase1)
