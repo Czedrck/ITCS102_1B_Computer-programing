ecc2e171afe65715063d914c527da74834348cc9
Czedrck <cedricklipata1@gmail.com>
2026-08-30 11:38:30 +0800

    Add files via upload

---
+++ codechallenge2.py
@@ -0,0 +1,57 @@
+#bank 
+
+Money = 19863	
+
+ T1 = 0 
+ T2 = 0
+ T3 = 0
+ T4 = 0
+ T5 = 0
+ T6 = 0
+ T7 = 0
+ T8 = 0
+ T9 = 0
+ 
+#1K, 500, 200, 100, 50, 20, 10, 5, 1 
+  
+  print("My current balance is", Money)		
+  T1 = Money//1000
+  Money = Money%1000
+  print("Thousands:", T1)    
+
+  T2 = Money//500
+  Money = Money%500
+  print("Thousands:", T2)
+
+  T3 = Money//200
+  Money = Money%200
+  print("Thousands:", T3)
+
+  T4 = Money//100
+  Money = Money%100
+  print("Thousands:", T4)
+
+  T5 = Money//50
+  Money = Money%50
+  print("Thousands:", T5)
+
+  T6 = Money//20
+  Money = Money%20
+  print("Thousands:", T6)
+
+  T7 = Money//10
+  Money = Money%10
+  print("Thousands:", T7)    
+
+  T8 = Money//5
+  Money = Money%5
+  print("Thousands:", T8)
+
+  T9 = Money//1
+  Money = Money%1
+  print("Thousands:", T9)
+
+  
+      
+       
+         
