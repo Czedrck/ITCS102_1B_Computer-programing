56207fe4adf7ad37ca85c9408f1c1b5106581f75
Czedrck <cedricklipata1@gmail.com>
2026-09-04 11:10:45 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,8 @@
+#input(), type(), int(), float()
+
+x = eval(input(Input a number -->")
+
+print(type(x)) #python function used to determine the data type of variables
+
+total = x + 5
+print("total = total")
---
+++ activity7.py
@@ -0,0 +1,18 @@
+total = 5	
+
+a = eval(input("Enter any number --> " ))
+total += a
+
+b = eval(input("Enter any number --> " ))
+total += b
+
+c = eval(input("Enter any number --> " ))
+total += c
+
+d = eval(input("Enter any number --> " ))
+total += d
+
+
+
+
+print("Current total is -->", total)
