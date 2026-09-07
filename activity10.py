a1fd30c65f785f8f134157fa8b3a5e0266ef6c35
Czedrck <cedricklipata1@gmail.com>
2026-09-07 11:20:55 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,13 @@
+#basic if else program	
+
+username = "Cedrick"
+password = "KaninBuseng"
+
+u = input("Input Username ---> ")
+p = input("Input Password ---> ")
+
+if u == username or p == password :
+        print("username and password is correct")
+else :
+        print("access denied")
+
---
+++ activity9.py
@@ -0,0 +1,20 @@
+#relational operator activity
+
+x = 5
+y = 5
+
+password1 = "Cedrick"
+password2 = "Lipata"
+
+#1
+print(x > y)
+#2
+print(x < y)
+#3 
+print(x >= y)
+#4
+print(x <= y)
+#5
+print(password1 == password2)
+#6 !=
+print(password1 != password2)
