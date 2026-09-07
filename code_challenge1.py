649ab7a0ab3e716e7de2c5695b5c8f9a2aa51bb4
Czedrck <cedricklipata1@gmail.com>
2026-09-07 10:24:10 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,12 @@
+#codechallenge1
+# \n = new line
+# \t = tab space
+# \r = carraige return\deprecated
+# \\ = insert slash
+# \" = insert double slash
+
+phrase1 = "\t\t\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\t* \t\t* \n\t\t\t\t\t\t\t\t\t\t\t* \t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t* \t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t* \t* \tHi my name is Jhon Cedrick Lipata \t* \t* \n\t\t\t\t\t\t\t\t\t* \t* \t\tFrom BSIT-1B \t\t\t* \t* \n\t\t\t\t\t\t\t\t\t*\t* \t\t\t\t\t\t* \t* \n\t\t\t\t\t\t\t\t\t*\t* \t\t\t\t\t\t* \t* \n\t\t\t\t\t\t\t\t\t*\t* \t* \t* \t* \t* \t* \t* \t* \n\t\t\t\t\t\t\t\t\t*\t* \t* \t* \t* \t* \t* \t* \t* "
+
+
+
+print(phrase1)
