c2c3f43d2810ad9f0e64941fb4f5ba976f9f8202
Czedrck <cedricklipata1@gmail.com>
2026-09-09 11:24:47 +0800

    Add files via upload

---
+++ codechallenge3.py
@@ -0,0 +1,10 @@
+#Global Freight Calculator
+
+name = input("Please Input the senders name ---> ")
+item = input("Please Input the item ---> ")
+
+weight = input("Please Input the weight ---> ")
+distance = input("Please Input the distance ---> ")
+
+
+
